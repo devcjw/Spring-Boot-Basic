73ff156e6810bc83a348ea793f234579c56cfef2
devcjw <devcjw@gmail.com>
2024-07-30 15:33:14 +0900

    Section 3

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,7 +4,8 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import
+        org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
@@ -15,8 +16,8 @@ public class MemberApp {
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
-
         Member findMember = memberService.findMember(1L);
+
         System.out.println("new member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
     }
